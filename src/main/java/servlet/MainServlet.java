1c4c6059232cb9a1e82faf988d68c11d60b219d1
Violetta Korolkova <la_bella.vita@mail.ru>
2023-06-20 19:55:55 +0200

    Initial commit

---
+++ src/main/java/servlet/MainServlet.java
@@ -0,0 +1,65 @@
+package servlet;
+
+import controller.PostController;
+import repository.PostRepository;
+import service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+
+private final static String METHOD_GET = "GET";
+    private final static String METHOD_POST = "POST";
+    private final static String METHOD_DELETE = "DELETE";
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(METHOD_GET) && path.equals("/api/posts")) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(METHOD_GET) && path.matches("/api/posts/\\d+")) {
+                // easy way
+                Long id = getId(path);
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(METHOD_POST) && path.equals("/api/posts")) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(METHOD_DELETE) && path.matches("/api/posts/\\d+")) {
+                // easy way
+              Long id = getId(path);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+
+
+    }
+
+
+    private Long getId(String path){
+        return Long.parseLong(path.substring(path.lastIndexOf("/")));
+    }
+}
